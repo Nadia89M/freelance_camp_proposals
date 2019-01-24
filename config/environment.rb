baac648990336a6a76d8c6f1612bc0189d7d1dd8
Nadia89M <nadia.mohamed89@gmail.com>
2019-01-24 14:20:10 +0000

    Setup Sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#ActionMailer
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'SENDGRID_USERNAME',
+  :password => 'SENDGRID_PASSWORD',
+  :domain => 'https://freelance-camp-proposals.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
